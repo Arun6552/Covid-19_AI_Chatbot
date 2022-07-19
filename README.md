a5e377faa4348b53181102a46f6501fcc51262cf
Arun Chaudhary <69865631+Arun6552@users.noreply.github.com>
2022-07-19 11:36:45 +0530

    docker file added

--- README.md
+++ README.md
@@ -1,1 +1,15 @@
 # Covid-19_AI_Chatbot
+
+
+## Software and Account Requirements 
+1. [Github Account](https://github.com/)
+2. [Heroku Account](https://id.heroku.com/login)
+3. [Dialogflow](https://dialogflow.cloud.google.com/)
+4. [VS Code IDE](https://code.visualstudio.com/download)
+5. [GIT CLI](https://git-scm.com/downloads)
+6. [Rapidapi](https://rapidapi.com/)
+
+
+```
+Step 1. Clone the Git clone 
+```
---
+++ app.py
@@ -0,0 +1,32 @@
+#Python module Import
+from flask import Flask, render_template
+from flask_cors import CORS, cross_origin
+import  os
+import  sys
+
+#Custom module Import
+from chatbot.exception import ChatbotException
+# from chatbot.logger import logging
+from chatbot.api_request import Api
+from chatbot.dialogflow_response import webhooks
+
+app = Flask(__name__)
+
+@app.route("/",methods=['GET','POST'])
+@cross_origin()
+def index():
+    return "Hello world!"
+
+@app.route('/results', methods=['GET','POST'])
+@cross_origin()
+def results():   
+    a =webhooks()
+    # logging.info("Running Successful!!!")
+    return a
+    
+
+if __name__ == '__main__':
+    port = int(os.getenv('PORT'))
+    print("Starting app on port %d" % port)
+    app.run(debug=False, port=port, host='0.0.0.0')
+    # app.run(debug=True)
